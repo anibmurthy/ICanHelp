e6f2bd36415bc1f9742dd3994cb0864b16c0c662
anibmurthy <ani4learning@gmail.com>
2021-03-01 12:31:41 -0500

    Working with 5.0

---
+++ ICanHelp/Platform/SQLiteDbContext.cs
@@ -0,0 +1,24 @@
+﻿using ICanHelp.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ICanHelp.Platform
+{
+    public class SQLiteDbContext :DbContext
+    {
+        public DbSet<SQLiteBridge> Boards { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=ICanHelp.db", options =>
+            {
+                options.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName);
+            });
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
